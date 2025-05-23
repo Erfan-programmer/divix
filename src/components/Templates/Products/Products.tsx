0bc1941e7ca8c2083aa18c4c2c175494e16029b5
Erfan-Programmer <erfankazemy1380@gmail.com>
2025-05-24 00:45:54 +0330

    fef

--- src/components/Templates/Products/Products.tsx
+++ src/components/Templates/Products/Products.tsx
@@ -501,7 +501,7 @@ const ProductsPage = () => {
               <p className="text-[#7A4522]/70">نتیجه‌ای یافت نشد</p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6">
               {products.map((product) => (
                 <ProductCard key={product.id} product={product} />
               ))}
